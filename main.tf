2e3a0b05fed97d6666f5880d5e6e278d0e7f7467
ShreyasAwankar <shreyasawankar01@gmail.com>
2023-12-18 20:23:36 +0530

    configuration changes

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "archive_file" "function_src" {
 resource "google_storage_bucket_object" "function_zip" {
   for_each = var.functions
   name     = each.key
-  bucket   = google_storage_bucket.bucket.name
+  bucket   = data.google_storage_bucket.bucket.name
   source   = data.archive_file.function_src[each.key].output_path
   # source = each.value.zip
 }
