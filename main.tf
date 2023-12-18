b38fbbbae19c16cace9bd6d9ed09f6ca38aae4b2
ShreyasAwankar <shreyasawankar01@gmail.com>
2023-12-18 19:55:11 +0530

    configuration changes

--- main.tf
+++ main.tf
@@ -1,89 +1,17 @@
-
-# data "google_storage_bucket" "existing_bucket" {
-#   count = var.create_bucket ? 0 : 1
-#   name  = "${var.project_id}-task5-bucket"
-# }
-
-# # Creating a storage bucket to store cloud function objects
-# resource "google_storage_bucket" "bucket" {
-#   project  = var.project_id
-#   name     = "${var.project_id}-task5-bucket"
-#   location = var.region
-# }
-
-
-# data "archive_file" "function_src" {
-#   for_each    = var.functions
-#   type        = "zip"
-#   output_path = "output/${each.key}.zip"
-#   source_dir  = "functions"
-# }
-
-# resource "google_storage_bucket_object" "function_zip" {
-#   for_each = var.functions
-#   name     = each.key
-#   bucket   = google_storage_bucket.bucket.name
-#   source   = data.archive_file.function_src[each.key].output_path
-#   # source = each.value.zip
-# }
-
-
-# resource "google_cloudfunctions2_function" "function" {
-#   for_each = var.functions
-#   name     = each.value.name
-#   location = var.region
-
-#   build_config {
-#     runtime     = each.value.runtime
-#     entry_point = each.value.entrypoint
-
-#     source {
-#       storage_source {
-#         bucket = google_storage_bucket.bucket.name
-#         object = google_storage_bucket_object.function_zip[each.key].name
-#       }
-#     }
-#   }
-#   service_config {
-#     min_instance_count             = 1
-#     available_memory               = "128Mi"
-#     timeout_seconds                = 120
-#     all_traffic_on_latest_revision = false
-#     service_account_email          = "terraform-gcf@terraform-cloud-functions-ems.iam.gserviceaccount.com"
-#   }
-# }
-
-# resource "google_cloud_run_service_iam_member" "member" {
-#   for_each = var.functions
-
-#   location = google_cloudfunctions2_function.function[each.key].location
-#   service  = each.key
-#   role     = "roles/run.invoker"
-#   member   = "allUsers"
-# }
-
-
-
-
-# ======================================================================
-# Configure resource count based on existing bucket
+# Check if bucket already exists
 data "google_storage_bucket" "existing_bucket" {
-  count = var.create_bucket ? 0 : 1
-  name  = "${var.project_id}-task5-bucket"
+  name = "${var.project_id}-task5-bucket"
 }
 
-# Create storage bucket if needed
+# Creating a storage bucket to store cloud function objects
 resource "google_storage_bucket" "bucket" {
-  count = data.google_storage_bucket.existing_bucket.count
-
-  # Accessing specific instance based on index
-  name = data.google_storage_bucket.existing_bucket[count.index].name
-
+  count    = data.google_storage_bucket.existing_bucket ? 0 : 1
   project  = var.project_id
+  name     = "${var.project_id}-task5-bucket"
   location = var.region
 }
 
-# Archive function source files
+
 data "archive_file" "function_src" {
   for_each    = var.functions
   type        = "zip"
@@ -91,15 +19,15 @@ data "archive_file" "function_src" {
   source_dir  = "functions"
 }
 
-# Upload function ZIPs to the bucket
 resource "google_storage_bucket_object" "function_zip" {
   for_each = var.functions
   name     = each.key
-  bucket   = google_storage_bucket.bucket[count.index].name
+  bucket   = google_storage_bucket.bucket.name
   source   = data.archive_file.function_src[each.key].output_path
+  # source = each.value.zip
 }
 
-# Deploy Cloud Functions
+
 resource "google_cloudfunctions2_function" "function" {
   for_each = var.functions
   name     = each.value.name
@@ -111,12 +39,11 @@ resource "google_cloudfunctions2_function" "function" {
 
     source {
       storage_source {
-        bucket = google_storage_bucket.bucket[count.index].name
+        bucket = google_storage_bucket.bucket.name
         object = google_storage_bucket_object.function_zip[each.key].name
       }
     }
   }
-
   service_config {
     min_instance_count             = 1
     available_memory               = "128Mi"
@@ -126,7 +53,6 @@ resource "google_cloudfunctions2_function" "function" {
   }
 }
 
-# Grant access to Cloud Functions for all users
 resource "google_cloud_run_service_iam_member" "member" {
   for_each = var.functions
 
@@ -135,3 +61,5 @@ resource "google_cloud_run_service_iam_member" "member" {
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
+
+
